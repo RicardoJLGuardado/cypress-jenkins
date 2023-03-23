e8c742dca94c50dddc038e366a8a912b43f17166
RicardoLopez <rjlopez@applaudostudios.com>
2023-03-22 21:42:37 -0600

    added the reporter library

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,20 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
+  screenshotOnRunFailure :true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/report',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
 
     baseUrl: "https://www.saucedemo.com/",
