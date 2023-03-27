7bc7a79dbc11eeb71cfbe4df47404004f0edb6bc
RicardoJLopez <rjlopez@applaudostudios.com>
2023-03-27 14:26:33 -0600

    browserstack integration and more custom start commands

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,20 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   screenshotOnRunFailure :true,
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir:'cypress/report',
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+  // reporter: 'cypress-mochawesome-reporter',
+  // reporterOptions: {
+  //   reportDir:'cypress/report',
+  //   charts: true,
+  //   reportPageTitle: 'custom-title',
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   saveAllAttempts: false,
+  // },
   e2e: {
     chromeWebSecurity: false,
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
+    // setupNodeEvents(on, config) {
+    //   require('cypress-mochawesome-reporter/plugin')(on);
+    // },
 
     baseUrl: "https://www.saucedemo.com/",
   },
